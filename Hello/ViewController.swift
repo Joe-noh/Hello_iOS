1a8a28cf0739b95af47f69f233b56ac86d18e7b1
Joe-noh <goflb.jh@gmail.com>
2016-06-19 15:34:06 +0900

    TapMeをタップするとコンソールにhelloを表示

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapMeButtonTapped(sender: UIButton) {
+        print("hello!!!")
+    }
 }
 
