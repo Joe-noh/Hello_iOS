25918d202fc9eb1ddd94782f667f8ee4e6edbf15
Joe-noh <goflb.jh@gmail.com>
2016-06-19 15:46:23 +0900

    ボタンを押すたびにテキストの色を変更

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -12,19 +12,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var helloTextField: UITextField!
     
+    let colors = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor()]
+    var currentIndex = 0;
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func tapMeButtonTapped(sender: UIButton) {
         helloTextField.text = "Hello!"
-        helloTextField.textColor = UIColor.redColor()
+        helloTextField.textColor = colors[currentIndex]
+        
+        currentIndex = (currentIndex + 1) % colors.count
     }
 }
 
