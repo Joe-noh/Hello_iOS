98bbad99730cb71764a344ec1c373b2ccbe09297
Joe-noh <honzawa.j@pepabo.com>
2016-06-23 09:24:49 +0900

    rename ViewController to HelloViewController

--- Hello/HelloViewController.swift
+++ Hello/HelloViewController.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HelloViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        print("hello")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
